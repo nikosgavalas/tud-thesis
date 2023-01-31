3fbabf7b175e3778f1b2701a85872ef9becf0b00
Nikos Gavalas <gavalnick@gmail.com>
2023-01-31 14:37:10 +0100

    minor change to test_fence

--- test/test_fence.py
+++ test/test_fence.py
@@ -9,8 +9,9 @@ class TestFencePointers(unittest.TestCase):
         rng = Random(1)
         fp1 = FencePointers()
         for i in range(100):
+            randbytes = rng.randbytes(rng.randint(50, 100))
             randint = rng.randint(0, 1000)
-            fp1.add(str(randint).encode(), randint)
+            fp1.add(randbytes, randint)
         fp2 = FencePointers()
         fp2.deserialize(fp1.serialize())
         self.assertEqual(fp1.pointers, fp2.pointers)
