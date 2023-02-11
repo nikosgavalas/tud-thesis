7f26c52f49ac1f41c0947e0047e6452826f6a1ab
Nikos Gavalas <gavalnick@gmail.com>
2023-02-11 22:35:43 +0100

    finish merging of runs lsm, add a couple of tests

--- src/lsmtree.py
+++ src/lsmtree.py
@@ -18,11 +18,6 @@ EMPTY = b''
 MAX_KEY_LENGTH = 256
 MAX_VALUE_LENGTH = 256
 
-# changable confs
-DATA_DIR = './data/'
-MAX_RUNS_PER_LEVEL = 3
-FENCE_POINTERS_SKIPS = 3  # NOTE this can be used to organize data into compressable blocks
-
 
 class Run:
     def __init__(self, pointers, filter):
@@ -31,26 +26,37 @@ class Run:
 
 
 class LSMTree:
-    def __init__(self, data_dir='./data', memtable_bytes_limit=1000000):
+    # NOTE the fence pointers can be used to organize data into compressable blocks
+    def __init__(self, data_dir='./data', max_runs_per_level=3, fence_pointer_skips=20, memtable_bytes_limit=1000000):
+        # TODO replace all refs to hardcoded 'data' dir to self.data_dir
         # TODO load data from WAL
-        self.data_dir = data_dir
+        self.data_dir = Path(data_dir)
+        self.data_dir.mkdir(exist_ok=True)
+
+        self.max_runs_per_level = max_runs_per_level
+        self.fence_pointer_skips = fence_pointer_skips
 
         self.memtable = SortedDict()
         self.memtable_bytes_limit = memtable_bytes_limit
         self.memtable_bytes_count = 0
 
         self.runs = []
-        
+
+        self.metadata = {
+            'num_runs': 0
+        }
+        self.metadata_path = self.data_dir / 'meta'
         self.load_metadata()
 
         # load filters and pointers for levels and runs
+        # TODO loading only first level for now
         for i in range(self.metadata['num_runs']):
-            with open(f'data/L0.{i}.pointers', 'r') as pointers_file:
+            with (self.data_dir / f'L0.{i}.pointers').open('r') as pointers_file:
                 data = pointers_file.read()
             pointers = FencePointers()
             pointers.deserialize(data)
 
-            with open(f'data/L0.{i}.filter', 'r') as filter_file:
+            with (self.data_dir / f'L0.{i}.filter').open('r') as filter_file:
                 data = filter_file.read()
             filter = BloomFilter()
             filter.deserialize(data)
@@ -58,36 +64,31 @@ class LSMTree:
             self.runs.append(Run(pointers, filter))
 
     def load_metadata(self):
-        metadata_path = Path('./data/metadata')
-        if metadata_path.is_file():
-            with metadata_path.open('r') as metadata_file:
+        if self.metadata_path.is_file():
+            with self.metadata_path.open('r') as metadata_file:
                 self.metadata = json.loads(metadata_file.read())
-        else:
-            self.metadata = {
-                'num_runs': 0
-            }
 
     def get(self, key: bytes):
         assert(type(key) is bytes and len(key) < MAX_KEY_LENGTH)
 
         if key in self.memtable:
             return self.memtable[key]
-        
+
         for i, run in reversed(list(enumerate(self.runs))):  # TODO for level in levels, ... for run in runs:... TODO remove enumerate, add the file in the Runs class?
             if key in run.filter:
                 idx = run.pointers.bisect(key) - 1  # -1 because I want the index of the item on the left
                 if idx < 0:
                     return EMPTY
                 _, offset = run.pointers.peekitem(idx)
-                with open(f'data/L0.{i}.run', 'rb') as run_file:
+                with (self.data_dir / f'L0.{i}.run').open('rb') as run_file:
                     run_file.seek(offset)
-                    for i in range(FENCE_POINTERS_SKIPS):
+                    for i in range(self.fence_pointer_skips):
                         read_key, read_value = self._read_kv_pair(run_file)
                         if read_key == key:
                             return read_value
 
         return EMPTY
-    
+
     def _read_kv_pair(self, fd):
         first_byte = fd.read(1)
         if not first_byte:
@@ -97,36 +98,40 @@ class LSMTree:
         val_len = struct.unpack('<B', fd.read(1))[0]
         value = fd.read(val_len)
         return key, value
-    
+
     def _write_kv_pair(self, fd, key, value):
         fd.write(struct.pack('<B', len(key)))
         fd.write(key)
         fd.write(struct.pack('<B', len(value)))
         fd.write(value)
-    
+
     def set(self, key: bytes, value: bytes = EMPTY):
-        assert(type(key) is bytes and type(value) is bytes and len(key) < MAX_KEY_LENGTH and len(value) < MAX_VALUE_LENGTH)
+        assert(type(key) is bytes and type(value) is bytes and len(key) < MAX_KEY_LENGTH and len(value) < MAX_VALUE_LENGTH) # TODO assert that key and value are non empty?
 
-        self.memtable[key] = value
-        self.memtable_bytes_count += len(key) + len(value)
+        self.memtable[key] = value  # NOTE maybe i should write after the flush? cause this way the limit is not a hard limit, it may be passed by up to 255 bytes
+        new_bytes_count = self.memtable_bytes_count + len(key) + len(value)
 
-        if self.memtable_bytes_count > self.memtable_bytes_limit:
+        if new_bytes_count > self.memtable_bytes_limit:
             # normally I would allocate a new memtable here so that writes can continue there
             # and then give the flushing of the old memtable to a background thread
 
-            self.flush_memtable()  # TODO make sure to flush at the end of each program somehow? If I had a server or REPL this would be easy
+            self.flush_memtable()  # TODO make sure to flush at the end of each program somehow? If I had a server or REPL this would be easy - no need now that i think about it cause of the WAL
 
-            if len(self.runs) == MAX_RUNS_PER_LEVEL:
+            if len(self.runs) == self.max_runs_per_level:
                 self.merge()
-                
+
             # TODO reset WAL
+        else:
+            # TODO write to WAL
+            self.memtable_bytes_count = new_bytes_count
     
     def merge(self):
+        # TODO create bloom filter and fence pointers for this!! see self.flush_memtable()
         fds = []
         keys = []
         values = []
-        for i in range(MAX_RUNS_PER_LEVEL):
-            fd = open(f'data/L0.{i}.run', 'rb')
+        for i in range(self.max_runs_per_level):
+            fd = (self.data_dir / f'L0.{i}.run').open('rb')
             fds.append(fd)
             k, v = self._read_kv_pair(fd)
             keys.append(k)
@@ -135,16 +140,16 @@ class LSMTree:
         # TODO does this assertion make sense?
         assert(b'' not in keys)
 
-        is_empty = [False for _ in range(MAX_RUNS_PER_LEVEL)]  # assuming no empty runs till here (see previous assertion)
-        with open('data/L1.0.run', 'wb') as run_file:
+        is_empty = [False for _ in range(self.max_runs_per_level)]  # assuming no empty runs till here (see previous assertion)
+        with (self.data_dir / 'L1.0.run' ).open('wb') as run_file:
             while not all(is_empty):
-                argmin_key = MAX_RUNS_PER_LEVEL - 1
-                # correctly initialize the argmin_key
-                for i in reversed(range(MAX_RUNS_PER_LEVEL)):
+                argmin_key = self.max_runs_per_level - 1
+                # correctly initialize the argmin_key (cause empty key b'' would make it instantly the argmin_key in the next for loop which we don't want)
+                for i in reversed(range(self.max_runs_per_level)):
                     if not is_empty[i]:
                         argmin_key = i
                         break
-                for i in reversed(range(MAX_RUNS_PER_LEVEL)):
+                for i in reversed(range(self.max_runs_per_level)):
                     if not is_empty[i] and keys[i] < keys[argmin_key]:
                         argmin_key = i
 
@@ -159,41 +164,47 @@ class LSMTree:
                     is_empty[argmin_key] = True
 
                 # skip duplicates
-                for i in reversed(range(argmin_key + 1)):
+                for i in reversed(range(argmin_key + 1)): # + 1 cause inclusive range
                     while not is_empty[i] and written_key == keys[i]:  # if it's the same key, read one more pair to skip it
                         keys[i], values[i] = self._read_kv_pair(fds[i])
                         if not keys[i]:
                             is_empty[i] = True
 
-    def flush_memtable(self):
+        for fd in fds:
+            fd.close()
+            # TODO empty the runs array
+            # TODO remove the files too after successfully merging.
 
-        with open(f'data/L0.{self.metadata["num_runs"]}.run', 'wb') as run_file:
+    def flush_memtable(self):
+        with (self.data_dir / f'L0.{self.metadata["num_runs"]}.run').open('wb') as run_file:
             kv_count = 0
             fence_pointers = FencePointers()
             filter = BloomFilter(len(self.memtable))
-            
+
             while self.memtable:
                 k, v = self.memtable.popitem(0)
 
-                if kv_count % FENCE_POINTERS_SKIPS == 0:
+                if kv_count % self.fence_pointer_skips == 0:
                     fence_pointers.add(k, run_file.tell())
                 kv_count += 1
 
                 self._write_kv_pair(run_file, k, v)
 
                 filter.add(k)
-            
+ 
+            self.memtable_bytes_count = 0
+
             self.runs.append(Run(fence_pointers, filter))
-        
-        with open(f'data/L0.{self.metadata["num_runs"]}.pointers', 'w') as pointers_file:
+
+        with (self.data_dir / f'L0.{self.metadata["num_runs"]}.pointers').open('w') as pointers_file:
             pointers_file.write(fence_pointers.serialize())
-        
-        with open(f'data/L0.{self.metadata["num_runs"]}.filter', 'w') as filter_file:
+
+        with (self.data_dir / f'L0.{self.metadata["num_runs"]}.filter').open('w') as filter_file:
             filter_file.write(filter.serialize())
-        
+
         self.metadata['num_runs'] += 1
         self.save_metadata()
- 
+
     def save_metadata(self):
-        with open('data/metadata', 'w') as metadata_file:
+        with self.metadata_path.open('w') as metadata_file:
             metadata_file.write(json.dumps(self.metadata))
--- test/test_lsmtree.py
+++ test/test_lsmtree.py
@@ -19,38 +19,36 @@ class TestLSMTree(unittest.TestCase):
         shutil.rmtree(self.dir.name)
     
     def test_e2e_1(self):
-        l = LSMTree(self.dir.name)
+        l = LSMTree(self.dir.name, max_runs_per_level=3, fence_pointer_skips=3, memtable_bytes_limit=10)
 
-        # l.set(b'b', b'2')
-        # l.set(b'asdf', b'12345')
-        # l.set(b'cc', b'cici345')
-        # l.set(b'b', b'3')
+        l.set(b'b', b'2')
+        l.set(b'asdf', b'12345')
+        l.set(b'cc', b'cici345')
+        l.set(b'b', b'3')
 
-        # l.flush_memtable()
+        self.assertEqual(l.get(b'b'), b'3')
+        self.assertEqual(l.get(b'asdf'), b'12345')
+        self.assertEqual(l.get(b'cc'), b'cici345')
 
-        # print(l.get(b'b'))
-        # print(l.get(b'asdf'))
-        # print(l.get(b'cc'))
+    def test_merge(self):
+        l = LSMTree(self.dir.name, max_runs_per_level=3, fence_pointer_skips=3, memtable_bytes_limit=10)
 
-        # l.merge()
+        l.set(b'a1', b'a1')
+        l.set(b'a1', b'a11')
+        l.set(b'a2', b'a2')
 
-    # def test_e2e_2(self):
-    #     l = LSMTree()
+        l.set(b'a2', b'a22')
+        l.set(b'a3', b'a3')
+        l.set(b'a4', b'a4')
 
-    #     l.set(b'a1', b'a1')
-    #     l.set(b'a1', b'a11')
-    #     l.set(b'a2', b'a2')
+        l.set(b'a3', b'a31')
+        l.set(b'a5', b'a5')
+        l.set(b'a6', b'a6')
 
-    #     l.set(b'a2', b'a22')
-    #     l.set(b'a3', b'a3')
-    #     l.set(b'a4', b'a4')
+        with (self.dir / 'L1.0.run').open('br') as f:
+            content = f.read()
 
-    #     l.set(b'a3', b'a31')
-    #     l.set(b'a5', b'a5')
-    #     l.set(b'a6', b'a6')
-
-    #     l.merge()
-    #     self.assertTrue(True)
+        self.assertEqual(content, b'\x02a1\x03a11\x02a2\x03a22\x02a3\x03a31\x02a4\x02a4\x02a5\x02a5\x02a6\x02a6')
 
 
 if __name__ == "__main__":
