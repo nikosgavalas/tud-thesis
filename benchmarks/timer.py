9f42b8e6dba264e71c2154b4c4633813c3415cf0
Nikos Gavalas <gavalnick@gmail.com>
2023-03-14 14:29:05 +0100

    add timer for experiments

---
+++ benchmarks/timer.py
@@ -0,0 +1,14 @@
+import time
+
+
+class Timer:
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.interval  = self.end - self.start
+
+    def __str__(self):
+        return f'{self.interval:.3f}'
