16c93335cd9f788faa5ee9d3b8b8186608789da0
Nikos Gavalas <gavalnick@gmail.com>
2023-05-05 23:49:39 +0200

    add more measurements

--- benchmarks/read_throughput.py
+++ benchmarks/read_throughput.py
@@ -3,34 +3,37 @@ from distributions import Uniform, Zipfian, HotSet
 from kevo import LSMTree, AppendLog, HybridLog, MemOnly, PathReplica
 
 
-def measure_throughput_latency_writes(db, distro, keys_set, vals_set, n_ops, engine, eng_comb, **args):
+def measure_throughput_latency_reads(db, distro, keys_set, vals_set, n_ops, engine, eng_comb, **args):
     res = []
 
     seconds = 10
-    writes_per_secs = list(range(10_000, 100_000, 10_000))
+    reads_per_secs = list(range(10_000, 100_000, 10_000))
 
-    for writes_per_sec in writes_per_secs:
+    # first populate
+    for k, v in zip(keys_set, vals_set):
+        db[k] = v
+
+    for reads_per_sec in reads_per_secs:
         latencies = []
         for i in range(seconds):
-            # pick key/values to write
             with Timer() as latency:
-                for _ in range(writes_per_sec):
-                    db[keys_set[next(distro)]] = vals_set[next(distro)]
+                for _ in range(reads_per_sec):
+                    _ = db[keys_set[next(distro)]]
             latency = float(latency)
-            avg_latency = latency / writes_per_sec
+            avg_latency = latency / reads_per_sec
             # if latency < 1:
             #     sleep(1 - latency)
             latencies.append(avg_latency)
 
-        res.append({'metric': '50p', 'value': percentile(latencies, 50), 'writes_per_sec': writes_per_sec/1000})
-        res.append({'metric': '95p', 'value': percentile(latencies, 95), 'writes_per_sec': writes_per_sec/1000})
+        res.append({'metric': '50p', 'value': percentile(latencies, 50), 'reads_per_sec': reads_per_sec/1000})
+        res.append({'metric': '95p', 'value': percentile(latencies, 95), 'reads_per_sec': reads_per_sec/1000})
 
     return res
 
 
 df = run(
     [5], [5], [10_000], [0],
-    [Zipfian], [{'seed': [1]}],
+    [Uniform], [{'seed': [1]}],
     [LSMTree, HybridLog, AppendLog], [{
         'max_runs_per_level': [10],
         'density_factor': [10],
@@ -51,15 +54,15 @@ df = run(
         'threshold': [10_000_000],
         'replica': [None]
     }],
-    [measure_throughput_latency_writes], {}
+    [measure_throughput_latency_reads], {}
 )
 
-data = df[['writes_per_sec', 'metric', 'value', 'engine']]
+data = df[['reads_per_sec', 'metric', 'value', 'engine']]
 data = data[data['metric'].isin(['50p', '95p'])]
 data['value'] = data['value'] * 1000000
 
-lineplot(data, 'writes_per_sec', 'value', 'write-throughput',
+lineplot(data, 'reads_per_sec', 'value', 'read-throughput',
          hue='engine',
          style='metric',
-         X='Throughput (Kwrites/sec)', Y='Latency (us)',
+         X='Throughput (Kreads/sec)', Y='Latency (us)',
          save=False, show=True)
--- benchmarks/snapshot.py
+++ benchmarks/snapshot.py
@@ -14,51 +14,101 @@ def measure_snapshot_time(db, distro, keys_set, vals_set, n_ops, engine, eng_com
     rng = Random(1)
     klen = 2
     vlen = 8
-    incr = 10_000  # has to match the mem of the engine in order to match the flush freq with the snapshot, to be fair
+    start = 10_000_000
+    incr = 10_000
+    n = 20
 
-    for i in range(args['n_boxes']):
+    size_so_far = 0
+    aggr_time = 0
+
+    for _ in range(incr):
+        db[rng.randbytes(klen)] = rng.randbytes(vlen)
+    with Timer() as t:
+        db.snapshot()
+    size_so_far += start + incr * (klen + vlen)
+    aggr_time += float(t)
+    row = {'metric': 'snapshot', 'value': float(t), 'write_volume': size_so_far / 1000000}
+    res.append(row)
+    row = {'metric': 'snapshot_aggr', 'value': aggr_time, 'write_volume': size_so_far / 1000000}
+    print(db.name, row)
+    res.append(row)
+
+    for _ in range(n):
         for _ in range(incr):
             db[rng.randbytes(klen)] = rng.randbytes(vlen)
         with Timer() as t:
             db.snapshot()
-        res.append({'metric': 'snapshot', 'value': float(t), 'n_boxes': (i+1) * (incr * (klen + vlen)) / 1000000})
+        size_so_far += start + incr * (klen + vlen)
+        aggr_time += float(t)
+        row = {'metric': 'snapshot', 'value': float(t), 'write_volume': size_so_far / 1000000}
+        res.append(row)
+        row = {'metric': 'snapshot_aggr', 'value': aggr_time, 'write_volume': size_so_far / 1000000}
+        print(db.name, row)
+        res.append(row)
 
     return res
 
-latency = 0#10**(-6)
+latency = 10**(-6)
 df = run(
     [2], [8], [1], [1],
     [Uniform], [{'seed': [1]}],
     [LSMTree, HybridLog, AppendLog, MemOnly], [{
-        'max_runs_per_level': [5],
+        'auto_push': [False],
+        'max_runs_per_level': [3],
         'density_factor': [10],
-        'memtable_bytes_limit': [20_000_000],
+        'memtable_bytes_limit': [1_000_000],
         'replica': [SimpleReplica('./benchmark_data_' + LSMTree.name, '/tmp/remote', network_latency_per_byte=latency)]
     },
     {
-        'max_runs_per_level': [5],
-        'mem_segment_len': [2_000_000],
-        'ro_lag_interval': [800_000],
-        'flush_interval': [100_000],
+        'auto_push': [False],
+        'max_runs_per_level': [3],
+        'mem_segment_len': [1_500_000],
+        'ro_lag_interval': [700_000],
+        'flush_interval': [700_000],
         'hash_index': ['dict'],
         'compaction_enabled': [False],
         'replica': [SimpleReplica('./benchmark_data_' + HybridLog.name, '/tmp/remote', network_latency_per_byte=latency)]
     },
     {
-        'max_runs_per_level': [5],
-        'threshold': [20_000_000],
+        'auto_push': [False],
+        'max_runs_per_level': [3],
+        'threshold': [1_000_000],
         'replica': [SimpleReplica('./benchmark_data_' + AppendLog.name, '/tmp/remote', network_latency_per_byte=latency)]
     },
     {
         'replica': [SimpleReplica('./benchmark_data_' + MemOnly.name, '/tmp/remote', network_latency_per_byte=latency)]
     }],
-    [measure_snapshot_time], {'n_boxes': 10}
+    [measure_snapshot_time], {}
 )
 
 
-data = df[['n_boxes', 'metric', 'value', 'engine']]
+# data = df[['write_volume', 'metric', 'value', 'engine']]
+# data = data[data['metric'].isin(['snapshot'])]
+#
+# barplot(data, 'write_volume', 'value', 'snapshot',
+#          hue='engine',
+#          X='Write Volume (MB)', Y='Snapshot time (s)',
+#          save=False, show=True)
+#
 
-barplot(data, 'n_boxes', 'value', 'snapshot',
-         hue='engine',
-         X='State Size (MB)', Y='Snapshot time (s)',
-         save=False, show=True)
+
+#
+# data = df[['write_volume', 'metric', 'value', 'engine']]
+# data = data[data['metric'].isin(['snapshot'])]
+#
+# lineplot(data, 'write_volume', 'value', 'snapshot',
+#          hue='engine',
+#          X='Write Volume (MB)', Y='Snapshot time (s)',
+#          save=False, show=True)
+#
+
+
+
+
+# data = df[['write_volume', 'metric', 'value', 'engine']]
+# data = data[data['metric'].isin(['snapshot_aggr'])]
+#
+# lineplot(data, 'write_volume', 'value', 'snapshot_aggr',
+#          hue='engine',
+#          X='Write Volume (MB)', Y='Snapshot time (s)',
+#          save=False, show=True)
