0ef6035fa71be7e6543de2fd6666e505b36e4b28
Nikos Gavalas <gavalnick@gmail.com>
2023-05-26 10:47:39 +0200

    fix packaging

--- kevo/__init__.py
+++ kevo/__init__.py
@@ -1,2 +1,2 @@
-from .engines import LSMTree, HybridLog, AppendLog, MemOnly
-from .remote import PathRemote, MinioRemote
+from kevo.engines import LSMTree, HybridLog, AppendLog, MemOnly
+from kevo.remote import PathRemote, MinioRemote
